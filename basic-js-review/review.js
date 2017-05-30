37291f92c827f18a99c3817f1a5832d82da30fe9
vannvaughan <vann.vaughan@yahoo.com>
2017-05-30 16:27:28 -0500

    completed

--- basic-js-review/review.js
+++ basic-js-review/review.js
@@ -1,30 +1,38 @@
 /////////////Variables/////////////
 //1. Write a variable called color with a value of "dark blue".
-
+var color = "dark blue"
 //2. Write a variable, myFave, that has the value "My favorite color is "
-
+var myFave = "My favorite color is "
 //3. Use the variables from 1 and 2 to make myFavoriteColor equal
 //   "My favorite color is dark blue" (concatenate)
-var myFavoriteColor;
+var myFavoriteColor = myFave + color;
 
 //4. Create an array of strings that includes the letters a - d in order.
-var aThroughD;
+var aThroughD = [ "a", "b", "c", "d"];
 //5. Remove 'a' and store it to a variable called isForApple.
-
+var isForApple = aThroughD.splice(0, 1);
+console.log(isForApple);
 //6. Add 'e' to the end of aThroughD.
-
+aThroughD.push("e");
 //7. Add 'apple' to the beginning of aThroughD
-
+aThroughD.unshift("apple");
 //8. Remove 'c' from the array and put 'cat' in its place
-
+aThroughD.splice(2, 1, "cat");
+console.log(aThroughD)
 //9. Leave 'apple' in the array but put a copy of it on a var called bestFruit
-
+var bestFruit = aThroughD.slice(0,1);
+console.log(bestFruit);
 //10. Create an object called spaceJam
 //    spaceJam has: a title of 'Space Jam' (String)
 //                  a release_year of 1996 (Number)
 //                  is not an oscarAwardWinner (Boolean)
 //                  and has the stars Michael Jordan, Bugs Bunny, and Bill Murray (array of strings)
-
+var spaceJam = {
+  title: 'Space Jam',
+  release_year: 1996,
+  oscarAwardWinner: false,
+  stars: ['Michael Jordan', 'Bugs Bunny', 'Bill Murray']
+}
 
 //11. Change anakin's side to dark using bracket notation
 
@@ -35,7 +43,7 @@ var anakinSkywalker =  {
   former_significant_other: 'Padme Amidala',
   former_mother: 'Shmi Skywalker'
 }
-
+anakinSkywalker['side'] = 'dark';
 //12. Add 'Jar Jar' to his children using dot notation
 
 //13. Loop through the object and remove any property that starts with former
